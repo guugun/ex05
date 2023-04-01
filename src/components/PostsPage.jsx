ed70f4cfa431840e1f218cc30d3d9c1937918ade
donggun <guugun@naver.com>
2023-04-01 10:21:21 +0900

    ver01

---
+++ src/components/PostsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PostsPage = () => {
+    return (
+        <div>게시글</div>
+    )
+}
+
+export default PostsPage
---
+++ src/components/TodosPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TodosPage = () => {
+    return (
+        <div>할일목록</div>
+    )
+}
+
+export default TodosPage
